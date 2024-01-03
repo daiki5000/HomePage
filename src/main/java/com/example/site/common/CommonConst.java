4ddf3f85359c327ac178658580378e9e3c7b2cc6
takeishi <twdia@takeishi>
2024-01-03 17:12:46 +0900

    [add][update]コントローラ修正

---
+++ src/main/java/com/example/site/common/CommonConst.java
@@ -0,0 +1,19 @@
+package com.example.site.common;
+
+public class CommonConst {
+
+	// トップ
+	public static final String TOP = "top";
+	
+	// 会社情報
+	public static final String COMPANY = "company";
+	
+	// 問い合わせ
+	public static final String CONTACT = "contact";
+	
+	// 求人
+	public static final String RECRUIT = "recruit";
+	
+	// サービス
+	public static final String SERVICE = "service";
+}
--- src/main/java/com/example/site/controller/HomePageController.java
+++ src/main/java/com/example/site/controller/HomePageController.java
@@ -4,12 +4,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.site.common.CommonConst;
+
 @Controller
 @RequestMapping
 public class HomePageController {
 
+	/**
+	 * トップページ
+	 */
 	@GetMapping
 	public String showTopPage() {
-		return "top";
+		return CommonConst.TOP;
+	}
+	
+	/**
+	 * 会社情報ページ
+	 */
+	@GetMapping(CommonConst.COMPANY)
+	public String showCompanyInfo() {
+		return CommonConst.COMPANY;
+	}
+	
+	/**
+	 * サービスページ
+	 */
+	@GetMapping(CommonConst.SERVICE)
+	public String showService() {
+		return CommonConst.SERVICE;
+	}
+	
+	/**
+	 * 求人ページ
+	 */
+	@GetMapping(CommonConst.RECRUIT)
+	public String showRecruit() {
+		return CommonConst.RECRUIT;
+	}
+	
+	/**
+	 * 問い合わせページ
+	 */
+	@GetMapping(CommonConst.CONTACT)
+	public String showContact() {
+		return CommonConst.CONTACT;
 	}
 }
